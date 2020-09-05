4cb2ba4ea112fe3d808014791aa4aa9b2fa76608
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2020-09-06 00:05:10 +0700

    Create validate feature

--- src/__tests__/damedane.js
+++ src/__tests__/damedane.js
@@ -1,4 +1,4 @@
-import { getByLabelText, getByTestId } from '@testing-library/dom'
+import { getByLabelText, getByTestId, fireEvent } from '@testing-library/dom'
 
 import damedane from '../damedane'
 
@@ -11,10 +11,12 @@ describe('Damedane', () => {
         <div class="form-group">
           <label for="username">Username</label>
           <input id="username" type="text" name="username" />
+          <p data-error="username" data-testid="error-username"></p>
         </div>
         <div class="form-group">
           <label for="password">Password</label>
           <input id="password" type="password" name="password" />
+          <p data-error="password" data-testid="error-password"></p>
         </div>
         <br />
         <button>Submit</button>
@@ -43,5 +45,81 @@ describe('Damedane', () => {
 
     expect(inputUsername.value).toBe('steve.fox')
     expect(inputPassword.value).toBe('mishimakiller')
+  })
+
+  it('should runs the validation rules', () => {
+    const $main = initDOM()
+    const $form = getByTestId($main, 'form')
+
+    const errorUsername = getByTestId($form, 'error-username')
+    const errorPassword = getByTestId($form, 'error-password')
+
+    expect(errorUsername).toHaveTextContent('')
+    expect(errorPassword).toHaveTextContent('')
+
+    const usernameIsRequired = (values) => {
+      const { username } = values
+
+      return {
+        name: 'username',
+        isError: !username.length,
+        message: 'Username is required'
+      }
+    }
+
+    const usernameHasInvalidLength = (values) => {
+      const { username } = values
+
+      return {
+        name: 'username',
+        isError: !(username.length >= 5 && username.length <= 10),
+        message: 'Username at least minimum 5 characters and maximum 10 characters'
+      }
+    }
+
+    const passwordIsRequired = (values) => {
+      const { password } = values
+
+      return {
+        name: 'password',
+        isError: !password.length,
+        message: 'Password is required'
+      }
+    }
+
+    const passwordHasInvalidLength = (values) => {
+      const { password } = values
+
+      return {
+        name: 'password',
+        isError: !(password.length >= 8 && password.length <= 50),
+        message: 'Password at least minimum 8 characters and maximum 50 characters'
+      }
+    }
+
+    damedane($form, {
+      values: {
+        username: '',
+        password: ''
+      },
+      rules: [
+        usernameIsRequired,
+        usernameHasInvalidLength,
+        passwordIsRequired,
+        passwordHasInvalidLength
+      ]
+    })
+
+    expect(errorUsername).toHaveTextContent('Username is required')
+    expect(errorPassword).toHaveTextContent('Password is required')
+
+    const inputUsername = getByLabelText($form, 'Username')
+    const inputPassword = getByLabelText($form, 'Password')
+
+    fireEvent.input(inputUsername, { target: { value: 'jin' } })
+    fireEvent.input(inputPassword, { target: { value: 'kazama' } })
+
+    expect(errorUsername).toHaveTextContent('Username at least minimum 5 characters and maximum 10 characters')
+    expect(errorPassword).toHaveTextContent('Password at least minimum 8 characters and maximum 50 characters')
   })
 })
--- src/damedane.js
+++ src/damedane.js
@@ -4,25 +4,70 @@
  * @param {HTMLFormElement} $form Form element.
  * @param {Object} options Used as configuration.
  * @param {Object} options.values Initial values.
+ * @param {Function[]} options.rules Rules used to validate values.
  */
 const damedane = ($form, options = {}) => {
-  // Values of form inputs
-  const values = { ...options.values }
+  // Values of form inputs.
+  const values = options.values ? { ...options.values } : {}
+
+  // Rules used to validate values.
+  const rules = options.rules ? [...options.rules] : []
 
   /**
    * Set default values to form inputs.
    */
   const initialize = () => {
-    const inputNames = Object.entries(values)
+    const valueEntries = Object.entries(values)
 
-    for (const [key, value] of inputNames) {
-      const $input = $form.querySelector(`input[name=${key}]`)
+    for (const [key, value] of valueEntries) {
+      const $input = $form.querySelector(`[name=${key}]`)
 
       $input.value = value
     }
   }
 
+  /**
+   * Validate values based on defined rules.
+   */
+  const validate = () => {
+    const errorRules = rules
+      .map(rule => rule(values))
+      .filter(rule => rule.isError)
+
+    Object
+      .keys(values)
+      .forEach(name => {
+        const rule = errorRules.find(rule => rule.name === name)
+        const $message = $form.querySelector(`[data-error=${name}]`)
+
+        if (!$message) return
+
+        $message.textContent = rule ? rule.message : ''
+      })
+  }
+
+  /**
+   * Listen everytime user change input values.
+   */
+  const listenChanges = () => {
+    const inputNames = Object.keys(values)
+
+    inputNames.forEach(name => {
+      const $input = $form.querySelector(`[name=${name}]`)
+
+      if ($input) {
+        $input.addEventListener('input', (event) => {
+          values[name] = event.target.value
+
+          validate()
+        })
+      }
+    })
+  }
+
   initialize()
+  validate()
+  listenChanges()
 }
 
 export default damedane
